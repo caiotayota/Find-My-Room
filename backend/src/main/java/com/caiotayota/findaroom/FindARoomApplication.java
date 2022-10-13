e3cca7b947e22a5da0bcce9a880a7575e2567985
--replace-all <caiotayota@live.com>
2022-10-13 22:24:34 +0100

    Initial commit

---
+++ backend/src/main/java/com/caiotayota/findaroom/FindARoomApplication.java
@@ -0,0 +1,13 @@
+package com.caiotayota.findaroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindARoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindARoomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/caiotayota/findaroom/FindARoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiotayota.findaroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindARoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
