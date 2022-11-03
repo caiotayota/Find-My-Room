a906f4d250a80f9ac850c4e6acf3f567a0eb5f88
--replace-all <caiotayota@live.com>
2022-11-03 23:24:45 +0000

    bCryptPasswordEncoder

--- backend/src/main/java/com/caiotayota/findaroom/FindARoomApplication.java
+++ backend/src/main/java/com/caiotayota/findaroom/FindARoomApplication.java
@@ -2,12 +2,18 @@ package com.caiotayota.findaroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class FindaroomApplication {
+public class FindARoomApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FindaroomApplication.class, args);
+		SpringApplication.run(FindARoomApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
