ade22050703e9925a36e6784a4598bbb4858f50b
--replace-all <caiotayota@live.com>
2022-11-18 01:27:09 +0000

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import './App.css'
+
+function App() {
+  return (
+    <>
+      <main>
+        <section id="homepage">
+          <h1>Find My Room</h1>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
