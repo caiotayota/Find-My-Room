05a389107a7a7a527a681cf129a0c7b6b673dda1
--replace-all <caiotayota@live.com>
2022-12-11 04:40:23 +0000

    Header Component

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,14 +1,12 @@
-import cover from '../../assets/img/cover14.jpg';
+import cover from '../../assets/img/cover-home.jpg';
 
 import './HeaderStyles.css';
 import Navbar from '../Navbar/Navbar';
-import SearchFilter from '../SearchFilter/SearchFilter';
 
 function Header() {
   return (
     <>
       <Navbar />
-      <SearchFilter />
       <div
         className="moving-clouds"
         style={{ backgroundImage: `url(${cover})` }}
